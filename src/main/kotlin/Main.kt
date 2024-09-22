216c0d850f5a76ef795cd8accbeda08a912ccc5d
Ral <econiqa@gmail.com>
2024-09-22 13:39:49 +0300

    homework 5 (updated) & homework 6 (not done yet)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,23 @@
-package com.ltrfsg
-
-val myInt: Int =
-val myFloat: Float = 0.58F
-val myLong: Long
-val myShort: Short
-val myByte: Byte = Byte
-val myDouble: Double =
-
-val isKotlinFun: Boolean = true
-
-val letter: Char = 'R'
-
-val text:String = "Hello Kotlin" //мб пустым
-
-val numbers: Array<Int> = arrayOf(1, 2, 3)
-val strings: MutableList<String> = mutableListOf("one")
-val doubles: Set<Double> = setOf(23.3)
-val keysToValues: Map<String, String> = mapOf(
-)
-val anytjing: Any = false //любой объект
-fun printMessage(message:String) { }
-fun printMessage(message:String): Nothing
+//package com.ltrfsg
+//
+//val myInt: Int =
+//val myFloat: Float = 0.58F
+//val myLong: Long
+//val myShort: Short
+//val myByte: Byte = Byte
+//val myDouble: Double =
+//
+//val isKotlinFun: Boolean = true
+//
+//val letter: Char = 'R'
+//
+//val text:String = "Hello Kotlin" //мб пустым
+//
+//val numbers: Array<Int> = arrayOf(1, 2, 3)
+//val strings: MutableList<String> = mutableListOf("one")
+//val doubles: Set<Double> = setOf(23.3)
+//val keysToValues: Map<String, String> = mapOf(
+//)
+//val anytjing: Any = false //любой объект
+//fun printMessage(message:String) { }
+//fun printMessage(message:String): Nothing
---
+++ src/main/kotlin/less5/lesson5part2.kt
@@ -0,0 +1,19 @@
+
+fun main () {
+    val initialIntense = 100.0
+    val DamplingCoeffknown: Double? = 0.7
+    val DamplingCoeffunknown: Double? = null
+    val intensityOfunknownCoeff = initialIntense, DamplingCoeffknown)
+    println(DamplingCoeffunknown)
+}
+
+
+fun main1 () {
+    var cargo: Double? = null
+    Price(90) Price (null)
+    val insurance = 0.5
+    val Insurance = (num ?: 50) * insurance / 100
+    var Price = (num ?: 50) + Insurance
+    println(Price)
+
+}
